bb8ceee010a66e06d86d0f769b3d75410a367326
Arnaud Golfouse <arnaud.golfouse@laposte.net>
2025-12-09 19:48:11 +0100

    Add pointer comparison in `Rc` test

--- tests/should_succeed/arc_and_rc.rs
+++ tests/should_succeed/arc_and_rc.rs
@@ -8,6 +8,17 @@ pub fn rc() {
     proof_assert!(*rc@ == 1i32);
     let inner = rc.as_ref();
     proof_assert!(inner@ == 1);
+
+    let rc2 = rc.clone();
+    proof_assert!(rc == rc2);
+
+    let b = Rc::ptr_eq(&rc, &rc2);
+    proof_assert!(b);
+
+    let rc3 = Rc::new(2);
+    let b2 = Rc::ptr_eq(&rc, &rc3);
+    // the content of the two rc is not the same, so their pointer have to be different
+    proof_assert!(!b2);
 }
 
 pub fn arc() {
